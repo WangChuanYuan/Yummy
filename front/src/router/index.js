518254713e4276a46574405bc2ca7ce0cb994a57
161250135 <161250135@smail.nju.edu.cn>
2019-01-28 22:15:54 +0800

    fix

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '@/views/Home';
-import Login from '@/views/Login';
 import Register from '@/views/Register';
 
 Vue.use(Router);
@@ -12,11 +11,6 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: Login
     },
     {
       path: '/register',
